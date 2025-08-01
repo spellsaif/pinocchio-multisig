e14a87d7e07dd18211dbbb4e518152b296e658b2
Saif <71248588+spellsaif@users.noreply.github.com>
2025-08-01 12:11:52 +0000

    included Transaction state

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -2,9 +2,11 @@ pub mod multisig;
 pub mod proposal;
 pub mod multisig_config;
 pub mod vote;
+pub mod transaction;
 
 
 pub use vote::*;
 pub use proposal::*;
 pub use multisig::*;
 pub use multisig_config::*;
+pub use transaction::*;
